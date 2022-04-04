a631ee5918ddcf92e0ca23ffaf9a241251a24b2c
fix <ivan.marevic@decode.agency>
2022-04-04 14:58:25 +0200

    changes for v2

--- src/App.js
+++ src/App.js
@@ -7,14 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Luka Gadže je direktor React developmenta <h1>YE BOI</h1>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
